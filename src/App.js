9d8b7d95ced9bfc700f46d53de3fa741c4f7315e
myano <lptakes276@icloud.com>
2019-10-07 04:08:08 +0900

    init2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Input from '@material-ui/core/Input';
+import {btn_clicked} from './Backend/CloudVision';
+import {picture_uploaded} from './Backend/CloudVision';
+import {readKey} from './Backend/CloudVision';
+import capture from './capture.png';
+//import {pictCapture_clicked} from './Backend/CloudVision';
+import CaptureForm from './CaptureForm';
 
-function App() {
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      isOpen:false,
+    };
+  } 
+  path = "C:\Users\Michitoshi\Pictures\sweetsApp\receipt";
+  onClick() {
+       
+      }
+  pictCapture_clicked() {
+    if(this.state.isOpen)
+    this.setState({isOpen:false});
+    else
+    this.setState({isOpen:true});
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img height = "300" width = "300" src = {capture} alt ="camera" onClick = {this.pictCapture_clicked.bind(this)}/>{
+      <CaptureForm isOpen = {this.state.isOpen} onClick = {this.pictCapture_clicked.bind(this)}/>}
+        <canvas id = "canvas"></canvas>
+        
+        <Button id='btnInput' color ="secondary" variant="contained" onClick = {btn_clicked.bind(this,this.path)}>Send</Button>
+        <Button id='btnRef' color ="secondary"  component="label">
+          Ref
+        <Input
+        className="inputter"
+        id = 'txtPicture'
+        type = 'file'
+        onChange = {picture_uploaded.bind(this)}
+        style={{display:'none'}} 
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+      />
+      </Button>
+      <Button id='btnKey' color ="secondary"  component="label">
+          UPLOAD_GCP_KEY
+        <Input
+        className="inputterKey"
+        id = 'txtKey'
+        type = 'file'
+        onChange = {readKey.bind(this)}
+        style={{display:'none'}} 
+        inputProps={{
+          'aria-label': 'Description',
+        }}
+      />
+      </Button>
       </header>
     </div>
-  );
+  );}
 }
-
 export default App;
